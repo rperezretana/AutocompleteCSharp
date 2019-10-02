7b68bf5d05c24640fb73d0fdd64aa6b04afc8cbe
Rolando Retana <rperezretana@hotmail.com>
2019-10-02 04:05:35 -0700

    Adding a site example

---
+++ WebApplicationExample/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public JsonResult AutoComplete(string prefix)
+        {
+            var emails = StaticCache.ContactsNameList.Search(prefix);
+            return Json(emails, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplicationExample/StaticCache.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoComplete;
+
+namespace WebApplicationExample
+{
+    public class StaticCache
+    {
+        public static AutoCompleteTree ContactsNameList { get; set; }
+    }
+}
